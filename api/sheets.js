e15bca0dc649a7107937e1a3e1fe103c86955c90
laurencetataren <laurence@thxgroup.com.br>
2026-03-30 05:21:12 -0300

    fix: increase maxDuration to 60s to avoid timeout

--- api/sheets.js
+++ api/sheets.js
@@ -457,7 +457,7 @@ function getMetas() {
 
 // Vercel: aumenta timeout para 30s (plano hobby suporta ate 60s em funcoes)
 export const config = {
-  maxDuration: 30
+  maxDuration: 60
 }
 
 export default async function handler(req, res) {
